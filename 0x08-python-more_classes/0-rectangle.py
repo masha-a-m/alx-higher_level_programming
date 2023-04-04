78fdb571b5cf68b450bbcd9995a1c31dbb3886b8
Maryam Garba <escritoramaree@gmail.com>
2023-04-04 14:21:02 -0700

    task0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This script defines a class for representing a rectangle."""
+
 class Rectangle:
-    """Represents a rectangle with a length and width."""
+    """This shows that it is representing  a rectangle."""
     pass
