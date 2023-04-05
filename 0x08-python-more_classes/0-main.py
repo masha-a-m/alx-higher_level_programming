ae83144e5ba3c5a155f62ac7bb99d2eb751847e1
Maryam Garba <escritoramaree@gmail.com>
2023-04-05 07:41:37 -0700

    task 0 main

---
+++ 0x08-python-more_classes/0-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+rectangle = __import__('0-rectangle').Rectangle
+"""asked to print my rectangle"""
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
