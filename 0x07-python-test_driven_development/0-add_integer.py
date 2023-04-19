db9d1c539fb56ec53ac1669dbcfa3bff99efa31a
Maryam Garba <escritoramaree@gmail.com>
2023-04-19 10:42:21 -0700

    task 2

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Defines an integer addition function."""
 def add_integer(a, b=98):
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    if ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-	 return (int(a) + int(b))
+    # Check if both a and b are integers or floats
+    if not (isinstance(a, int) or isinstance(a, float)) or not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    # a and b to integers if they are floats
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
+
+    # Add integers to return the result
+    return a + b
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,31 @@
+def matrix_divided(matrix, div):
+    """
+    Divides all elements of a matrix by a given number.
+
+    Parameters:
+    matrix (list): A list of lists of integers or floats representing the matrix
+    div (int or float): The number to divide all elements of the matrix by
+
+    Returns:
+    list: A new matrix with all elements divided by the given number and rounded to 2 decimal places
+    """
+    # Check if matrix is a list of lists of integers or floats
+    if not all(isinstance(row, list) and all(isinstance(elem, (int, float)) for elem in row) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    # Check if each row of the matrix has the same size
+    if len(set(len(row) for row in matrix)) > 1:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    # Check if div is a number
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    # Check if div is not 0
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    # Divide each element of the matrix by div and round to 2 decimal places
+    new_matrix = [[round(elem/div, 2) for elem in row] for row in matrix]
+
+    return new_matrix
