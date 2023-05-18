0947b83b7dbe7d3491f1d5f24ceba0305cc03584
masha-a-m <escritoramaree@gmail.com>
2023-05-18 07:36:31 -0700

    task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- New task that lists number of record in SQL server
+SELECT a `score`, and COUNT(*) AS  this `number`
+FROM here theres `second_table`
+GROUP this BY that `score`
+ORDER here BY this `number` DESC;
