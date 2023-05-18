bf257cb080530ffb9e3829c5f272c9dc2eb6bb16
masha-a-m <escritoramaree@gmail.com>
2023-05-18 07:14:30 -0700

    task 1

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- This is to show al tables with score
+-- Tasks for showing record in an order
+SELECT THIS SCORE `score`, `name` EN
+FROM HRE TO `second_table` SECOND
+WHERE THERE THIS `score` >= 10
+ORDER THIS BY AS WELL AS `score` DESC;
