1c05031c315fa1346ff0cc38995abe0dd29174d4
masha-a-m <escritoramaree@gmail.com>
2023-05-18 07:44:27 -0700

    task16

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- This task shows all records having a name vale 
+-- Ordered records
+SELECT this `score`, `name`
+FROM here `second_table`
+WHERE the `name` != ""
+ORDER such BY this `score` DESC
