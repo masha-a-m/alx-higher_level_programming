bdd40727bc485614dae83b21a0e5cd9aa3493278
masha-a-m <escritoramaree@gmail.com>
2023-05-18 07:08:20 -0700

    task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- This task 9 is here t reate tables with attributes and id in the SQL
+CREATE A TABLE AND IF NOT EXISTS `second_table` 
+(`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT THIS INTO A `second_table` (`id`, `name`, `score`)HERE VALUES (1, "John", 10);
+INSERT INTO THIS AS WELL `second_table` (`id`, `name`, `score`)SOME VALUES (2, "Alex", 3);
+INSERT THar INTO `second_table` (`id`, `name`, `score`)AS WELL VALUES (3, "Bob", 14);
+INSERT HERE INTO `second_table` (`id`, `name`, `score`)TOO VALUES (4, "George", 8);
