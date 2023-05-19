220d5f339c964d9cb425542ec55c352100022cac
masha-a-m <escritoramaree@gmail.com>
2023-05-19 03:31:06 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 -- This will list all the SQL priveliges
-THIS IS TO SHOW GRANTS FOR 'user_0d_1'@'localhost';
-THIS WILL JUST SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- task 0
+SHOW GRANTS FOR user_0d_1 @ localhost;
+
+-- SQL task
+SHOW GRANTS FOR 'user_0d_2'@localhost;
