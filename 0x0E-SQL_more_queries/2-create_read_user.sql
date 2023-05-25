2c23ad79eeb10fd8a10939d42db360edcdbd6967
masha-a-m <escritoramaree@gmail.com>
2023-05-24 17:51:55 -0700

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,6 @@
 --fatsbase xreation
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
-
+--new alx task 2 
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-
-
+--task SQL create database
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
