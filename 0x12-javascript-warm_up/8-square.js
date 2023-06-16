2a5ab8905ff0429cafdeb7b10aaf34ba082843b7
masha-a-m <escritoramaree@gmail.com>
2023-06-16 13:31:30 -0700

    u

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+//consta size math floor
+
+
+const size = Math.floor(Number(process.argv[2])); //process argv.w
+
+
+if (isNaN(size)) 
+{
+  console.log('Missing size');
+} 
+e
+lse 
+
+{
+  fo r (let r = 0; r < size; r++)  /for let r+0
+
+
+  {
+    let row = ''; //letr row
+
+    for (let c = 0; c < size; c++) row += 'X'; //rpw +='X';
+
+
+    console.log(row);
+  }
+}
