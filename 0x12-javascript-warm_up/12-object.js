13324ab36526baddae2aba390ebc66220d37ead8
masha-a-m <escritoramaree@gmail.com>
2023-06-16 11:31:27 -0700

    u

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const myObject =
+{
+  type: 'object', //task for alx java
+
+  value: 12 //value is 12
+
+};
+
+console.log(myObject);
+
+//my object
+
+myObject.value = 89;
+
+console.log(myObject);
