c9211016531ceece31afffe4644c6e39f10ae265
masha-a-m <escritoramaree@gmail.com>
2023-06-16 13:33:06 -0700

    u

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// function a+b
+
+function add (a, b) //function add a+b
+{
+  return a + b; //return a+b
+}
+
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3]))); //console add number 
