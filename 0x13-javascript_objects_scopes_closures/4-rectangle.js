c5f9a64abbaa57fe98e60f1278876f7125412c1a
masha-a-m <escritoramaree@gmail.com>
2023-06-17 08:14:34 -0700

    u

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,57 @@
+#!/usr/bin/node 
+
+// task 3 java script
+
+
+class Rectangle //class of rectangle 
+{
+  constructor (w, h)
+
+	   // constructor w, h
+  {
+    if ((w > 0) && (h > 0)) //if w> 0, h) 0
+
+
+    {
+      this.width = w;
+
+//this width = w
+      this.height = h;
+
+      //this height = h
+
+
+    }
+  }
+
+  print () //print ()
+
+  {
+    for (let i = 0; i < this.height; i++)  //for let i =0; i < this height
+
+
+    {
+      let s = ''; //let s = '';
+
+      for (let j = 0; j < this.width; j++) //for let j = 0;
+      {
+        s += 'X';
+      }
+      console.log(s);
+    }
+  }
+
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+
+  double () {
+    this.width *= 2;
+    this =m 2
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
