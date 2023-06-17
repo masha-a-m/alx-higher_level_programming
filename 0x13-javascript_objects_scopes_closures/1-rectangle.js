d45fdbe2cbb1d4cec5af131d006855aa6434f34b
masha-a-m <escritoramaree@gmail.com>
2023-06-17 07:59:13 -0700

    u

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+//task 1 rectangle java
+
+
+
+class Rectangle 
+
+{
+  constructor (w, h)  // constructor w, h
+
+  {
+    this.width = w; //this width = w
+
+
+    this.height = h; //this height = h
+
+  }
+}
+
+module.exports = Rectangle; //module exports reectangle
