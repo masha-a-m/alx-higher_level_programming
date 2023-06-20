ad5336297c39f2e21b68011d89e2dea9e921a651
masha-a-m <escritoramaree@gmail.com>
2023-06-19 21:38:12 -0700

    u

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,8 @@
 """ task 1 statibg all stqtes that begin with n"""
 
 
-"""importation of sys"""
-
 import sys 
+
 import MySQLdb
 
 """then my SQL"""
@@ -14,13 +13,11 @@ if __name__ == '__main__':
 
 """password, database, username"""
 
-    db = MySQLdb.connect(user=sys.argv[1] 
-		      passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv{1}, passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
-    cur = db.cursor()
     cur.execute()
 	 """cur execute"""
+    
     states = cur.fetchall()
 
     for state in states:
