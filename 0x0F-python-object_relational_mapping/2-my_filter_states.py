7fd5ebca213a1e2b41b5b9f5787eef125c4290a1
masha-a-m <escritoramaree@gmail.com>
2023-06-19 21:47:49 -0700

    u

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -2,20 +2,23 @@
 
 """task 2 python"""
 
-db = MySQLdb.connect(user=sys.argv[1], passwd=sys.a    rgv[2],
 
 import mySQLdb
 
 import sys
-
-if __name__ == '__main__':
-                         db=sys.argv[3], port=3306)
 
 
     cur.execute("SELECT * \
     FROM states \
     WHERE CONVERT(`name` USING Latin1) \
     COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+
 
 cur = db.cursor()
 
