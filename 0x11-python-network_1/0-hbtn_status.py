05ab16843dc6c7bc49069ee134479efa52598c76
masha-a-m <escritoramaree@gmail.com>
2023-07-04 06:13:59 -0700

    u

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,4 +7,13 @@ if __name__ == '__main__':
 #pyton 0x11 
     import urllib.request
     urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+# Fetch the URL
+with urllib.request.urlopen(url) as response:
+    # Read the response data
+    body = response.read().decode('utf-8')
+
+# Display the body of the response
+print(f"Response body:\n{'-' * 50}\n{body}\n{'-' * 50}")content = res.read()
