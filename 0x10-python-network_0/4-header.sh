7dd95b97b93990fb24688d78767ede04e58fa197
masha-a-m <escritoramaree@gmail.com>
2023-07-06 09:54:28 -0700

    u

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#cript that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the responsA header variable X-School-User-Id must be sent with the value 9You have to use curlPlease test your script in the sandbox provided, using the
+curl -sH "X-HolbertonSchool-User-Id: 98" "${1}"
