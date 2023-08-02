3b25b7baa8c56797f4302f32546ca9a267d23c60
masha-a-m <escritoramaree@gmail.com>
2023-08-02 01:14:26 -0700

    u

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
-
-"""python file liating task 0"""
-
-
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 import sys
-
-"""import the sys from """
-
-
 import MySQLdb
 
-"""include three arguments """
-
-
 if __name__ == '__main__':
-
-"""database, name, username, password """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-
-    """sys argv port"""
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-
-    """cursor db cir """
-
     cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
 
-    for state in state:
-
-    """for the state """
-
+    for state in states:
         print(state)
-
-
